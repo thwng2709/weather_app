25e90b444c623a5de4f2eac1adebc11b95bb8969
thwng <thwng2709@gmail.com>
2025-03-07 18:29:31 +0700

    second push, requesting location permission

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,17 +1,31 @@
 package com.example.weatherapp
 
+import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.location.LocationManager
+import android.net.Uri
 import android.os.Bundle
+import android.os.Looper
 import android.provider.Settings
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY
 
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_LOCATION_CODE = 1
+    private lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,6 +36,8 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
         if (!isLocationEnabled()) {
             Toast.makeText(
                 this@MainActivity,
@@ -31,12 +47,41 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
             startActivity(intent)
         } else {
-            Toast.makeText(
-                this@MainActivity,
-                "The location is already enabled",
-                Toast.LENGTH_SHORT
-            ).show()
+            requestPermission()
         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+        deviceId: Int
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults, deviceId)
+        if (requestCode == REQUEST_LOCATION_CODE && grantResults.size > 0) {
+            Toast.makeText(this@MainActivity, "Permission granted", Toast.LENGTH_SHORT).show()
+            requestLocationData()
+        } else {
+            Toast.makeText(this@MainActivity, "Permission denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestLocationData() {
+        val locationRequest = LocationRequest.Builder(PRIORITY_HIGH_ACCURACY, 1000).build()
+        mFusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            object : LocationCallback() {
+                override fun onLocationResult(locationResult: com.google.android.gms.location.LocationResult) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Lattitude ${locationResult.lastLocation?.latitude} \nLongitude ${locationResult.lastLocation?.longitude}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            },
+            Looper.myLooper()
+        )
     }
 
     private fun isLocationEnabled(): Boolean {
@@ -44,5 +89,47 @@ class MainActivity : AppCompatActivity() {
             getSystemService(Context.LOCATION_SERVICE) as LocationManager
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
                 || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        ) {
+            showRequestDialog()
+        } else if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        ) {
+            requestPermission()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                REQUEST_LOCATION_CODE
+            )
+        }
+    }
+
+    private fun showRequestDialog() {
+        AlertDialog.Builder(this)
+            .setPositiveButton("GO TO SETTINGS") { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }.setNegativeButton("CLOSE") { dialog, _ -> dialog.cancel() }
+            .setTitle("Location permission needed")
+            .setMessage("This permission is needed for accessing location. It can be enabled under the application settings")
+            .show()
     }
 }
